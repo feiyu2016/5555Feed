01471d333ffb01f614173db57628c16a5641d7b9
Christian Fjeld <fjeld.christian@gmail.com>
2014-04-19 15:41:15 +0200

    Add FeedApplication.java

---
+++ src/no/fjeld/feed/FeedApplication.java
@@ -0,0 +1,10 @@
+package no.fjeld.feed;
+
+public class FeedApplication extends android.app.Application {
+
+    public android.app.ActionBar mActionBar;
+    public NavigationDrawer mNavDrawer;
+    public android.webkit.WebView mWebView;
+
+}
+
