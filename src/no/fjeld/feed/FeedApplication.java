a89a2d1d6274bf3061e9f15d14f537aceca1ccf7
Christian Fjeld <fjeld.christian@gmail.com>
2014-04-19 15:52:22 +0200

    Modify FeedApplication.java

--- src/no/fjeld/feed/FeedApplication.java
+++ src/no/fjeld/feed/FeedApplication.java
@@ -2,9 +2,34 @@ package no.fjeld.feed;
 
 public class FeedApplication extends android.app.Application {
 
-    public android.app.ActionBar mActionBar;
-    public NavigationDrawer mNavDrawer;
-    public android.webkit.WebView mWebView;
+    private static FeedApplication singleton;
+
+    private android.app.ActionBar mActionBar;
+    private NavigationDrawer mNavDrawer;
+    private android.webkit.WebView mWebView;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        singleton = this;
+    }
+
+    public FeedApplication getInstance() {
+        return singleton;
+    }
+
+    public android.app.ActionBar getActionBar() {
+        return mActionBar;
+    }
+
+    public NavigationDrawer getNavDrawer() {
+        return mNavDrawer;
+    }
+
+    public android.webkit.WebView getWebView() {
+        return mWebView;
+    }
+
 
 }
 
