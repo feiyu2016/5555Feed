1301e8a8ab73bda45569101545f773e467326de0
Christian Fjeld <fjeld.christian@gmail.com>
2014-04-19 14:24:59 +0200

    Add FeedActivity.java

---
+++ src/no/fjeld/feed/FeedActivity.java
@@ -0,0 +1,36 @@
+package no.fjeld.feed;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.support.v4.app.*;
+import android.support.v4.widget.*;
+import android.view.*;
+import android.widget.*;
+import android.widget.AdapterView.*;
+
+public class FeedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+
+        super.onPostCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+}
