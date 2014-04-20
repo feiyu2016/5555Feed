2a150667269d038b59a8a07d2e28f2b5eca70fe4
Christian Fjeld <fjeld.christian@gmail.com>
2014-04-20 13:06:29 +0200

    Add Feed.java

---
+++ src/no/fjeld/feed/Feed.java
@@ -0,0 +1,31 @@
+package no.fjeld.feed;
+
+import java.util.*;
+
+import android.app.*;
+import android.content.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.net.*;
+import android.os.*;
+import android.preference.*;
+import android.view.*;
+import android.view.animation.*;
+import android.widget.*;
+import android.widget.AdapterView.*;
+
+public class Feed {
+
+    /**
+     * Constructor for the class Feed.
+     *
+     * @param view The FeedActivity content view.
+     * @param mApp The Application-object for this app.
+     */
+    public Feed(View view, FeedApplication mApp) {
+
+
+    }
+
+
+}
