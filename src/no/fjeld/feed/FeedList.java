4bbf5cd1787986012877446b00147ab4da2b2121
Christian Fjeld <fjeld.christian@gmail.com>
2014-05-12 21:56:48 +0200

    Add case if the item is a saved item
    If an item is saved, even if it is marked as
    read in another list, it should still appear
    in the list of saved items.

--- src/no/fjeld/feed/FeedList.java
+++ src/no/fjeld/feed/FeedList.java
@@ -10,25 +10,39 @@ public class FeedList extends ArrayList <FeedItem> {
      */
     private static final long serialVersionUID = -6728285211029336860L;
     public ArrayList <String> readItems;
+    public ArrayList <FeedItem> savedItems;
 
     @Override
     public boolean add(FeedItem item) {
-        
-        int i = 0;
+
+        /* If the item is a saved item. */
+        if (item.getFeed() == null) 
+            super.add(item);
+
+        int i = 0, j = 0, k = 0;
         boolean added = false;
+        boolean looping = true;
 
-        /* If the item is marked as read, don't add it. */
-        for (i = 0; i < readItems.size(); i++)
-            if (item.getUrl().equals(readItems.get(i)))
-                return false;
+        while(looping) {
 
-        /* If the item is already in the list, don't add it. */
-        for (i = 0; i < super.size() && i < 20; i++) 
-            if (item.getUrl().equals(super.get(i).getUrl())) 
-                return false;
-             
-        /* Adds the items by date. */
-        for (i = 0; i < super.size() && i < 20; i++) {
+            if (i == super.size() && j == readItems.size() && k == savedItems.size())
+                looping = false;
+
+            if (i < super.size())
+                if (item.getUrl().equals(super.get(i++).getUrl()))
+                    return false;
+
+            if (j < readItems.size())
+                if (item.getUrl().equals(readItems.get(j++)))
+                    return false;
+
+            if (k < savedItems.size())
+                if (item.getUrl().equals(savedItems.get(k++).getUrl()))
+                    return false;
+
+        }
+
+        for (i = 0; i < super.size(); i++) {
             if (item.compareTo(super.get(i)) >= 0) {
                 super.add(i, item);
                 added = true;
@@ -36,7 +50,7 @@ public class FeedList extends ArrayList <FeedItem> {
             }
         }
 
-        if (!added && i < 20) 
+        if (!added) 
             super.add(item);
 
         return true;
