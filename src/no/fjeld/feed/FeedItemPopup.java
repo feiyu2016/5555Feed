09dbf6737a59437e1d7c57f1d24d990b36d09624
Christian Fjeld <fjeld.christian@gmail.com>
2014-05-23 15:16:17 +0200

    Add FeedItemPopup.java
    This class displays the popup that will
    happen when the user taps on a list item
    in mFeedList.

---
+++ src/no/fjeld/feed/FeedItemPopup.java
@@ -0,0 +1,30 @@
+package no.fjeld.feed;
+
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.util.*;
+import android.view.*;
+import android.view.animation.*;
+import android.view.ViewGroup.*;
+import android.widget.*;
+
+public class FeedItemPopup {
+
+    private FeedApplication mApp;
+    private FeedItem mFeedItem;
+
+    public FeedItemPopup(FeedApplication app, FeedItem item) {
+
+        this.mApp = app;
+        this.mFeedItem = item;
+
+    }
+
+    public void initView() {
+
+
+    }
+
+}
