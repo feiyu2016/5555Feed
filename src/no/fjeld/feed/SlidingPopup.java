42319effce59691ae3a3ce6defc6020c2e967ded
Christian Fjeld <fjeld.christian@gmail.com>
2014-06-23 22:42:56 +0200

    Add the main functionality in SlidingPopup.java
    The class adds a fading background and slides the
    provided child view down from the top of the screen.
    If a ClickEvent occures on the background, the View
    slides out the top of the screen.

--- src/no/fjeld/feed/SlidingPopup.java
+++ src/no/fjeld/feed/SlidingPopup.java
@@ -1,13 +1,161 @@
 package no.fjeld.feed;
 
+import android.app.Activity;
 import android.view.*;
+import android.view.View.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 
 public class SlidingPopup {
 
+    private final static int ANIMATION_TIME = 300;
+
+    private static SlidingPopup sInstance;
+    private static boolean sVisible;
+
+    private Activity mActivity;
+
+    private ViewGroup mParentView;
+    private View mFadingView;
     private View mChildView;
 
-    public SlidingPopup(View childView) {
+    private TransitionDrawable mTransition;
+
+    /**
+     * Public constructor for SlidingPopup.
+     *
+     * @param activity   An Activity-pointer.
+     * @param parentView The ViewGroup which will show the sliding View.
+     * @param childView  The View that will slide down.
+     */
+    public SlidingPopup(Activity activity, ViewGroup parentView, 
+            View childView) {
+
+        mActivity = activity;
+
+        mParentView = parentView;
         mChildView = childView;
+
+        mTransition = getTransitionDrawable();
+
+        mFadingView = new View(mActivity); 
+        mFadingView.setBackground(mTransition);
+
+        mParentView.addView(mFadingView);
+        mParentView.addView(mChildView);
+
+        sInstance = this;
+
+        setCancelListener();
+        slideDown();
+
+    } 
+
+    /**
+     * Starts the fading (transition) of the background
+     * and animates mChildView (sliding in).
+     */
+    private void slideDown() {
+
+        Animation anim = AnimationUtils.loadAnimation(
+                mActivity, R.anim.slide_in_top); 
+        
+        mChildView.startAnimation(anim);
+        mTransition.startTransition(ANIMATION_TIME);
+        
+        sVisible = true;
+
+    }
+
+    /**
+     * Reverse the fading (transition) of the background
+     * and animates mChildView (sliding out).
+     */
+    private void slideUp() {
+
+        Animation anim = AnimationUtils.loadAnimation(
+                mActivity, R.anim.slide_out_top); 
+        anim.setAnimationListener(new SlideOutListener());
+
+        mChildView.startAnimation(anim);
+        mTransition.reverseTransition(ANIMATION_TIME);
+
+    }
+
+    /**
+     * Creates a new TransitionDrawable for use as a fading 
+     * background when a View slides down from the top of 
+     * the screen.
+     *
+     * @return A new TransitionDrawable.
+     */
+    private TransitionDrawable getTransitionDrawable() {
+
+        ColorDrawable layerOne = new ColorDrawable(
+                android.R.color.transparent);
+        ColorDrawable layerTwo = new ColorDrawable(
+                Color.parseColor("#99000000"));
+
+        return new TransitionDrawable(new Drawable [] {
+            layerOne, layerTwo
+        });
+
+    }
+
+    /**
+     * Sets an OnTouchListener on mFadingView that cancels
+     * the View when a TouchEvent has occured on the background.
+     */
+    private void setCancelListener() {
+
+        mFadingView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                slideUp();
+            }
+        });
+
+    }
+
+    /** 
+     * Returns the current instance of the visible View.
+     */
+    public static SlidingPopup getInstance() {
+        return sInstance;
+    }
+
+    /**
+     * Returns true or false depending on if the View is 
+     * visible or not.
+     */ 
+    public static boolean isVisible() {
+        return sVisible;
+    }
+
+    /**
+     * AnimationListener for the animation that slides the
+     * View out of the screen.
+     */
+    private class SlideOutListener implements AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {}
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {}
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+            mParentView.removeView(mFadingView);
+            mParentView.removeView(mChildView);
+            sVisible = false; 
+
+        }
+
     }
 
 }
