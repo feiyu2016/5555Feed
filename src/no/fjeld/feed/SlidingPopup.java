ad3c4cce0f28f37eeb5b88db23426f26faa7322d
Christian Fjeld <fjeld.christian@gmail.com>
2014-06-23 20:45:10 +0200

    Add SlidingPopup.java

---
+++ src/no/fjeld/feed/SlidingPopup.java
@@ -0,0 +1,13 @@
+package no.fjeld.feed;
+
+import android.view.*;
+
+public class SlidingPopup {
+
+    private View mChildView;
+
+    public SlidingPopup(View childView) {
+        mChildView = childView;
+    }
+
+}
