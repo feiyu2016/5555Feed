6865fa73cf5c33f481685d7ea0bd19859a8f2862
Christian Fjeld <fjeld.christian@gmail.com>
2014-06-24 13:35:27 +0200

    Add comments to SlidingFeedItem.java

--- src/no/fjeld/feed/SlidingFeedItem.java
+++ src/no/fjeld/feed/SlidingFeedItem.java
@@ -11,12 +11,19 @@ public class SlidingFeedItem {
 
     private final static int SHARE = 0;
     private final static int READ = 1;
-    private int mAction;
+    private int mAction = -1;
 
     private Activity mActivity;
     private ViewGroup mParentView;
     private FeedItem mFeedItem;
 
+    /**
+     * Public constructor for SlidingFeedItem.
+     *
+     * @param activity   An activitypointer. 
+     * @param parentView The ViewGroup that this View should be displayed in.
+     * @param feedItem   The FeedItem that contains the values for the View.
+     */
     public SlidingFeedItem(Activity activity, ViewGroup parentView, 
             FeedItem feedItem) {
 
@@ -24,11 +31,29 @@ public class SlidingFeedItem {
         mParentView = parentView;
         mFeedItem = feedItem;
 
-        initChildView();
+        initView();
 
     }
 
-    private void initChildView() {
+    /**
+     * Calls the functions that initializes the necessary values
+     * and objects for the sliding view.
+     */
+    private void initView() {
+
+        LinearLayout mainView = getView();
+        setOnClickListeners(mainView);
+        initSlidingView(mainView);
+
+    }
+
+    /**
+     * Creates the View that displays the FeedItem's image, title,
+     * and description.
+     *
+     * @return mainView The new View with the FeedItem's values.
+     */    
+    private LinearLayout getView() {
 
         LayoutInflater inflater = (LayoutInflater) mActivity
             .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -43,22 +68,22 @@ public class SlidingFeedItem {
         TextView description = (TextView) mainView.findViewById(
                 R.id.popup_description);
 
-        image.setImageBitmap(mFeedItem.getImage());
+        if (mFeedItem.getImage() == null)
+            image.setVisibility(View.GONE);
+        else 
+            image.setImageBitmap(mFeedItem.getImage());
+
         title.setText(mFeedItem.getTitle());
         description.setText(mFeedItem.getDescription());
 
-        setOnClickListeners(mainView);
-
-        new SlidingView(mActivity, mParentView, mainView) {
-            @Override
-            public void onSlideOutFinished() {
-                if      (mAction == SHARE) share();
-                else if (mAction == READ)  read(); 
-            }   
-        };
+        return mainView;
 
     } 
 
+    /**
+     * Sets OnClickListeners for the buttons (Share and Read) in
+     * the View.
+     */
     private void setOnClickListeners(LinearLayout mainView) {
 
         mainView.findViewById(R.id.popup_share).setOnClickListener(
@@ -81,29 +106,54 @@ public class SlidingFeedItem {
 
     }
 
+    /**
+     * Creates a new instance of the SlidingView class.
+     * It also overrides 'onSlideOutFinished()' in SlidingView
+     * in order to execute the correct function when a button
+     * has been clicked.
+     */
+    private void initSlidingView(LinearLayout mainView) {
+
+        new SlidingView(mActivity, mParentView, mainView) {
+            @Override
+            public void onSlideOutFinished() {
+                if      (mAction == SHARE) share();
+                else if (mAction == READ)  read(); 
+            }   
+        };
+
+    }
+
     private void share() {
 
     }
 
+    /**
+     * Opens either a WebView or the default browser based on
+     * the users preferences.
+     * It displays the article from the url that the FeedItem
+     * contains.
+     */
     private void read() {
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
-                mActivity);
+        SharedPreferences prefs = PreferenceManager
+            .getDefaultSharedPreferences(mActivity);
 
         if (prefs.getBoolean("preference_open_in", true)) {
 
             Intent appIntent = new Intent(mActivity,
-                    WebViewActivity.class).putExtra("url", mFeedItem.getUrl());
+                    WebViewActivity.class).putExtra(
+                        "url", mFeedItem.getUrl());
             mActivity.startActivity(appIntent);
 
         } else {
 
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW,
-                    Uri.parse(mFeedItem.getUrl()));
+            Intent browserIntent = new Intent(
+                    Intent.ACTION_VIEW, Uri.parse(mFeedItem.getUrl()));
             mActivity.startActivity(browserIntent);
 
         }
-    
+
     }
 
 }
