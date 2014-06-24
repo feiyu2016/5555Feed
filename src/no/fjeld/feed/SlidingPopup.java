234bb0fd46d1319de1203f8f8626850244543c38
Christian Fjeld <fjeld.christian@gmail.com>
2014-06-24 10:54:30 +0200

    Add comments to SlidingPopup.java

--- src/no/fjeld/feed/SlidingPopup.java
+++ src/no/fjeld/feed/SlidingPopup.java
@@ -9,6 +9,26 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 
+/**
+ * Applies a background that fades into a darker color while
+ * sliding a View down from the top of the screen.
+ * 
+ * A click on the background will slide the View out of the
+ * screen again. If the backbutton should be used for the 
+ * same purpose, put the following in the activitiy that 
+ * uses the View:
+ *
+ * @Override
+ * public void onBackPressed() {
+ *     if (SlidingPopup.getInstance() != null)
+ *         SlidingPopup.getInstance().slideOut();
+ *     else
+ *         super.onBackPressed();
+ * }
+ *
+ * If any functionality is desired when the View has slided
+ * out of the screen, override 'onSlideOutFinished()'.
+ */
 public class SlidingPopup {
 
     private final static int ANIMATION_TIME = 300;
@@ -34,7 +54,7 @@ public class SlidingPopup {
 
         sInstance = this;
         mTransition = getTransitionDrawable();
-        
+
         mActivity = activity;
 
         mParentView = parentView;
@@ -58,7 +78,7 @@ public class SlidingPopup {
 
         Animation anim = AnimationUtils.loadAnimation(
                 mActivity, R.anim.slide_in_top); 
-        
+
         mChildView.startAnimation(anim);
         mTransition.startTransition(ANIMATION_TIME);
 
