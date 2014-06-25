2db9c596331e0549af7d9a249b3ce4980ebdca48
Christian Fjeld <fjeld.christian@gmail.com>
2014-06-25 11:12:52 +0200

    Restructure code in SwipeTouchListener.java

--- src/no/fjeld/feed/SwipeTouchListener.java
+++ src/no/fjeld/feed/SwipeTouchListener.java
@@ -34,14 +34,17 @@ public class SwipeTouchListener implements OnTouchListener {
     private int mSwipeSlop;
 
     /**
-     * Constructor for the class SwipeTouchListener.
+     * Public constructor for the class SwipeTouchListener.
      *
      * @param activity   An Activity-pointer. 
      * @param viewParent The ViewGroup that contains the View. 
      */
-    public SwipeTouchListener(Activity activity, ViewGroup viewParent) {  
+    public SwipeTouchListener(Activity activity, ViewGroup 
+            viewParent) {  
+
         mActivity = activity;
         mViewParent = viewParent;
+
     }
 
     /**
@@ -50,68 +53,48 @@ public class SwipeTouchListener implements OnTouchListener {
      * when the user performs certain actions.
      *
      * @param view  The View that recieved the touch event
-     * @param event A MotionEvent-object containing info about the event 
+     * @param event A MotionEvent-object containing info about
+     *              the event 
      */
     @Override
     public boolean onTouch(View view, MotionEvent event) {
 
-        mSwipeSlop = ViewConfiguration.get(mActivity)
-                .getScaledTouchSlop();
+        mSwipeSlop = ViewConfiguration
+                .get(mActivity).getScaledTouchSlop();
 
         switch (event.getAction()) {
 
-            /* First touch on the View. */
+            /* The View has received its first TouchEvent. */ 
             case MotionEvent.ACTION_DOWN:
-
                 if (mViewPressed)
                     return false;
-
                 mViewPressed = true;
 
-                if (mVelocityTracker == null)
-                    mVelocityTracker = VelocityTracker.obtain();
-                else
-                    mVelocityTracker.clear();
-
-                mVelocityTracker.addMovement(event);
-
-                mView = view;
-                mDownX = event.getX();
-
+                actionDown(view, event);
                 break;
 
-            /* The View is dragged. */
+            /* The View has been dragged to a location. */
             case MotionEvent.ACTION_MOVE: 
-
-                mVelocityTracker.addMovement(event);
-                mVelocityTracker.computeCurrentVelocity(100);
-
                 actionMove(event);
-
                 break;
 
-            /* The View is released. */
+            /* The hold on the View has been released. */ 
             case MotionEvent.ACTION_UP: 
-
                 mViewPressed = false;
 
                 if (mSwiping)
                     onSwipe(event);
                 else 
                     return onClick(); 
-
                 break;
 
-            /* The hold on the View has been cancelled 
-             * for some reason. */
+            /* The hold on the View has been cancelled. */ 
             case MotionEvent.ACTION_CANCEL:
-
                 mViewPressed = false;
                 mVelocityTracker = null;
 
                 view.setAlpha(1);
                 view.setTranslationX(0);
-
                 break;
 
         }
@@ -120,15 +103,33 @@ public class SwipeTouchListener implements OnTouchListener {
 
     }
 
+    private void actionDown(View view, MotionEvent event) {
+
+        if (mVelocityTracker == null)
+            mVelocityTracker = VelocityTracker.obtain();
+        else
+            mVelocityTracker.clear();
+
+        mVelocityTracker.addMovement(event);
+
+        mView = view;
+        mDownX = event.getX();
+
+    }
+
     /**
-     * Is called when the user drags the view.
+     * Is called from 'onTouch()' when the View has been dragged 
+     * to a location. 
      * 
      * @param event The MotionEvent-object from onTouch()
      */
     private void actionMove(MotionEvent event) {
 
+        mVelocityTracker.addMovement(event);
+        mVelocityTracker.computeCurrentVelocity(100);
+
         float currentX = event.getX() + mView.getTranslationX();
-        float deltaX = currentX - mDownX;
+        float deltaX   = currentX - mDownX;
 
         if (!mSwiping) {
             if (Math.abs(deltaX) > mSwipeSlop) {
@@ -145,95 +146,98 @@ public class SwipeTouchListener implements OnTouchListener {
     }
 
     /**
-     * Is called when the user has swiped the view.
+     * Is called from 'onTouch()' when the View has been released
+     * and a swipe-gesture has been performed on the View. 
      * 
      * @param event The MotionEvent-object from onTouch() 
      */
     private void onSwipe(MotionEvent event) {
 
-        float currentX = event.getX() + mView.getTranslationX();
-        float deltaX = currentX - mDownX;
+        float currentX  = event.getX() + mView.getTranslationX();
+        float deltaX    = currentX - mDownX;
         float velocityX = Math.abs(mVelocityTracker.getXVelocity());
 
         float outOfView, endX, endAlpha;
         boolean dismiss = false;
 
-        /* The user has performed a swipe-gesture at the specified velocity, or over 
-        *  a third of the Views width has been dragged out of its initial position. */
-        if (velocityX > SWIPE_VELOCITY_MIN || Math.abs(deltaX) > mView.getWidth() / 3) {
+        /* The user has performed a swipe-gesture at the specified 
+         * velocity, or over a third of the Views width has been 
+         * dragged out of its initial position. */
+        if (velocityX > SWIPE_VELOCITY_MIN || Math.abs(deltaX) 
+                > mView.getWidth() / 3) {
 
             outOfView = Math.abs(deltaX) / mView.getWidth();
-            endX = deltaX < 0 ? -mView.getWidth() : mView.getWidth();
-            endAlpha = 0;
-            dismiss = true;
+            endX      = (deltaX < 0)
+                        ? -mView.getWidth() 
+                        : mView.getWidth();
+            endAlpha  = 0;
+            dismiss   = true;
 
-        /* The user has released the hold on the View before it should be dismissed. */
+        /* The user has released the hold on the View before it 
+         * should be dismissed. */
         } else {
 
             outOfView = 1 - (Math.abs(deltaX) / mView.getWidth());
-            endX = 0;
-            endAlpha = 1;
+            endX      = 0;
+            endAlpha  = 1;
 
         }
 
         mViewParent.setEnabled(false);
 
         long duration = (int) ((1 - outOfView) * SWIPE_DURATION);
+        endSwipe(duration, endAlpha, endX, dismiss);
+
+    }
+
+    private void endSwipe(long duration, float endAlpha, final float 
+            fEndX, final boolean fDismiss) {
 
         mView.animate().setDuration(duration)
-                .alpha(endAlpha).translationX(endX)
-                .setListener(new SlideOutListener(dismiss, endX));
+                .alpha(endAlpha).translationX(fEndX)
+                .setListener(new AnimatorListener() {
+
+                @Override
+                public void onAnimationEnd(Animator anim) {
+
+                    mView.setAlpha(1);
+                    mView.setTranslationX(0);
+                    mSwiping = false;
+                    mViewParent.setEnabled(true);
+
+                    if (fDismiss) {
+                        if (fEndX < 0) onSwipeLeft();
+                        else           onSwipeRight();
+                    }
+                
+                }
+            
+                @Override
+                public void onAnimationCancel(Animator anim) {}
+
+                @Override
+                public void onAnimationRepeat(Animator anim) {}
+
+                @Override
+                public void onAnimationStart(Animator anim) {}
+
+            });
 
     }
 
-    /** Is called when the user has performed a single click on the View. */
+    /** 
+     * Is called when the user has performed a single click on the View. 
+     */
     public boolean onClick() { return true; }
 
-    /** Is called when the user has performed a left swipe on the View. */
+    /** 
+     * Is called when the user has performed a left swipe on the View. 
+     */
     public void onSwipeLeft() {}
 
-    /** Is called when the user has performed a right swipe on the View. */
-    public void onSwipeRight() {}
-
-    /**
-     * The AnimatorListener for the animation that is executed on the View 
-     * after it has been swiped. 
+    /** 
+     * Is called when the user has performed a right swipe on the View. 
      */
-    private class SlideOutListener implements AnimatorListener {
-
-        private boolean mDismiss;
-        private float mEndX;
-
-        public SlideOutListener(boolean dismiss, float endX) {
-            mDismiss = dismiss;
-            mEndX = endX;
-        }
-
-        @Override
-        public void onAnimationEnd(Animator anim) {
-
-            mView.setAlpha(1);
-            mView.setTranslationX(0);
-
-            mSwiping = false;
-            mViewParent.setEnabled(true);
-
-            if (mDismiss) { 
-                if (mEndX < 0) onSwipeLeft();
-                else onSwipeRight(); 
-            }
-
-        }
-
-        @Override
-        public void onAnimationCancel(Animator anim) {}
-        
-        @Override
-        public void onAnimationRepeat(Animator anim) {}
-
-        @Override
-        public void onAnimationStart(Animator anim) {}
-   
-    }
+    public void onSwipeRight() {}
 
 }
